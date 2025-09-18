91756d20c310686b2698add75c6e817b4cb6b546
Damian-Oswald <damian.oswald@protonmail.com>
2025-09-18 19:22:22 +0200

    Convert AGIS crops

---
+++ scripts/agis.R
@@ -0,0 +1,72 @@
+#' =============================================================================
+#'       title:  AGIS CROP TABLE CONVERSION TO RDF
+#'      author:  Damian Oswald
+#'        date:  2025-09-18
+#' =============================================================================
+
+library(rdfhelper)
+
+#' =============================================================================
+#' DOWNLOAD DATA
+#' =============================================================================
+
+url <- "https://www.blw.admin.ch/dam/de/sd-web/bkAU6T83hyLT/LWB_Nutzungsfl%C3%A4chen_Kataloge.xlsx"
+
+destfile <- tempfile(fileext = ".xlsx")
+download.file(url, destfile, mode = "wb")
+data <- readxl::read_excel(destfile, sheet = 1)
+
+#' =============================================================================
+#' PREFIX DEFINITION
+#' =============================================================================
+
+base <- "https://agriculture.ld.admin.ch/crops/"
+schema  <- "http://schema.org/"
+
+#' =============================================================================
+#' TABLE TO RDF CONVERSION
+#' =============================================================================
+
+sink("rdf/crops.ttl")
+
+for (i in seq_len(nrow(data))) {
+  LNF <- as.integer(data[i, "LNF_Code"])
+  subject <- rdfhelper::uri(LNF, prefix = base)
+  rdfhelper::triple(subject, "a", rdfhelper::uri("CropGroup", base))
+  for (lang in c("de", "fr", "it")) {
+    rdfhelper::triple(
+      subject = subject,
+      predicate = rdfhelper::uri("name", schema),
+      object = rdfhelper::langstring(
+        x = data[i, paste0("Nutzung_", toupper(lang))],
+        lang = lang
+      )
+    )
+  }
+
+  keys <- c(Spezialkultur = "SpecialtyCrop", BFF_QI = "BFFQ1")
+  for (j in seq_along(keys)) {
+    if (as.integer(data[i, names(keys[j])]) == 1) {
+      rdfhelper::triple(subject, "a", rdfhelper::uri(keys[j], base))
+    }
+  }
+
+  keys <- c(
+    Gueltig_Von = rdfhelper::uri("validFrom", schema),
+    Gueltig_Bis = rdfhelper::uri("validTo", schema)
+  )
+  for (j in seq_along(keys)) {
+    x <- as.integer(data[i, names(keys[j])])
+    if (!is.na(x)) {
+      rdfhelper::triple(subject, keys[j], rdfhelper::typed(x, "gYear"))
+    }
+  }
+
+  rdfhelper::triple(
+    subject,
+    rdfhelper::uri("LNF", base),
+    as.integer(data[i,"LNF_Code"])
+  )
+}
+
+sink()
