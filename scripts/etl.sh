5322d05088e3db7d5328184b4637100c30ec2921
Damian-Oswald <damian.oswald@protonmail.com>
2025-10-01 16:55:09 +0200

    Improve etl pipeline

--- scripts/etl.sh
+++ scripts/etl.sh
@@ -1,4 +1,26 @@
-Rscript scripts/agis.R
-Rscript scripts/grud.R
-Rscript scripts/provar.R
+#!/bin/bash
+
+# Load environment variables from .env file
+source .env
+
+# Run R processing steps
+for r in agis grud provar; do
+  Rscript "scripts/${r}.R"
+done
+
+# Process RDF files using Python scrips
 python3 scripts/reason.py rdf/ontology.ttl rdf/agis.ttl rdf/srppp.ttl rdf/grud.ttl rdf/provar.ttl
+
+# Delete existing data from LINDAS
+curl \
+  --user $USER:$PASSWORD \
+  -X DELETE \
+  "$ENDPOINT?graph=$GRAPH"
+
+# Upload graph.ttl to LINDAS 
+curl \
+  --user $USER:$PASSWORD \
+  -X POST \
+  -H "Content-Type: text/turtle" \
+  --data-binary @rdf/graph.ttl \
+  "$ENDPOINT?graph=$GRAPH"
