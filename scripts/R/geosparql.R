1b180b214ad4364cf725268638cdea6a73b0055b
Damian-Oswald <damian.oswald@protonmail.com>
2025-12-07 20:38:28 +0100

    Improve processing

--- scripts/R/geosparql.R
+++ scripts/R/geosparql.R
@@ -5,56 +5,82 @@
 library(sf)
 library(rdfhelper)
 
-# 1. Read data
+# Read data
 # The source is Swiss LV95 (EPSG: 2056). 
 data <- sf::st_read("data/data.gpkg", layer = "nutzungsflaechen")
 
-# 2. Transform CRS to WGS 84 (EPSG: 4326) for GeoSPARQL compliance
+# Transform CRS to WGS 84 (EPSG: 4326) for GeoSPARQL compliance
 # This converts the Swiss coordinates to Longitude/Latitude
 data <- sf::st_transform(data, 4326)
+
+# Filter only some cantons
+data <- subset(data, subset = kanton %in% c("TG", "ZH"))
 
 # Prefix Definitions
 base <- "https://agriculture.ld.admin.ch/crops/"
 cultivation <- paste0(base, "cultivation/")
 cultivationtype <- paste0(base, "cultivationtype/")
+programs <- paste0(base, "programs/")
 geosparql <- "http://www.opengis.net/ont/geosparql#"
 
+# Get cantons
+cantons <- sparql('
+PREFIX ch: <https://schema.ld.admin.ch/>
+PREFIX schema: <http://schema.org/>
+SELECT * WHERE {
+  ?canton a ch:Canton ; schema:alternateName ?code .
+}
+', 'https://lindas.admin.ch/query')
+
+# Create a new file
 sink("rdf/geodata.ttl")
 
-# loop over data
+# Loop over data
 for (i in seq_len(nrow(data))) {
+
+  # select only the *i*th observation
+  x <- data[i, ]
+  
+  # define subject IRI
+  subject <- uri(x$t_id, cultivation)
+  
+  # class and cultivationtype assignment
+  triple(subject, "a", uri("Cultivation", base))
+  triple(subject, uri("cultivationtype", base), uri(x$lnf_code, cultivationtype))
   
   # Extract geometry as WKT string from the transformed data
   # We use st_geometry() to be safe, regardless of what the column is named
-  wkt_string <- sf::st_as_text(sf::st_geometry(data)[[i]])
-  
-  subject <- rdfhelper::uri(data$t_id[i], cultivation)
-  
-  rdfhelper::triple(subject, "a", rdfhelper::uri("Cultivation", base))
-  
-  rdfhelper::triple(
+  triple(
     subject,
-    rdfhelper::uri("cultivationtype", base),
-    rdfhelper::uri(data$lnf_code[i], cultivationtype)
+    uri("asWKT", geosparql),
+    typed(sf::st_as_text(sf::st_geometry(x)), "wktLiteral") 
   )
   
-  # The wktLiteral will now contain standard WGS84 Lat/Long coordinates
-  rdfhelper::triple(
+  # Area
+  triple(
     subject,
-    rdfhelper::uri("asWKT", geosparql),
-    rdfhelper::typed(wkt_string, "wktLiteral") 
+    uri("area", base),
+    x$flaeche_m2
   )
-  
-  rdfhelper::triple(
+
+  # Direct payment programmes
+  # triple(subject, uri("program", base), uri(unlist(strsplit(x$code_programm, ";")), programs) )
+
+  # Number of trees
+  # triple(subject, uri("trees", base), x$anzahl_baeume)
+
+  # Responsible canton
+  triple(
     subject,
-    rdfhelper::uri("area_m2", base),
-    data$flaeche_m2[i]
+    uri("canton", base),
+    uri(subset(cantons, subset = code==x$kanton, select = canton))
   )
-  
-  rdfhelper::triple(
+
+  # Mowing Date
+  triple(
     subject,
-    rdfhelper::uri("trees", base),
-    data$anzahl_baeume[i]
+    uri("movingDate", base),
+    typed(x$schnittzeitpunkt, "date")
   )
 }
 
