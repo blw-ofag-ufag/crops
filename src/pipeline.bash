0c70fa2f953f748cd8dbfae0662d3895d1e366d5
Damian-Oswald <damian.oswald@protonmail.com>
2026-03-05 17:22:50 +0100

    Add processed OWL file optimizied for webvowl visualization of taxonomy

--- src/pipeline.bash
+++ src/pipeline.bash
@@ -7,11 +7,11 @@ echo "Validate syntax of turtle files"
 python src/python/validate.py rdf
 
 
-echo "Create a dedicated ontology file for subsequent WebVOWL visualization"
+echo "Create a dedicated OWL file for subsequent WebVOWL visualization of the crop taxonomy"
 python src/python/rdf-processing.py \
-  --input rdf/ontology/*.ttl \
-  --output rdf/processed/ontology.ttl \
-  --rules src/sparql/inference-rules/*.sparql src/sparql/processing-rules/*.sparql
+  --input rdf/ontology/cultivationtypes.ttl \
+  --output rdf/processed/crop-taxonomy.ttl \
+  --rules src/sparql/processing-rules/*.sparql
 
 
 echo "Merge all data into one graph for subsequent LINDAS upload"
