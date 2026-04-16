d02d5624a4653f51ceb3e01b7c09d2f2803ed0a7
Damian-Oswald <damian.oswald@protonmail.com>
2026-04-16 12:41:53 +0200

    Integrate version info, handle duplicates

--- src/r/psmv-integration.R
+++ src/r/psmv-integration.R
@@ -38,6 +38,13 @@ for (i in seq_len(nrow(crops))) {
     rdfhelper::typed(id, "ID")
   )
 
+  # Infofito concept version
+  rdfhelper::triple(
+    uri,
+    rdfhelper::uri("http://schema.org/version"),
+    rdfhelper::typed(data[i, "VERSION"], "integer")
+  )
+
   # labels
   for (lang in c("en", "de", "fr", "it")) {
     value <- subset(
@@ -48,7 +55,7 @@ for (i in seq_len(nrow(crops))) {
     rdfhelper::triple(
       uri,
       rdfhelper::uri("http://schema.org/name"),
-      rdfhelper::langstring(as.character(value), lang)
+      rdfhelper::langstring(as.character(unique(value)), lang)
     )
   }
 
@@ -57,8 +64,15 @@ for (i in seq_len(nrow(crops))) {
   parents <- unlist(subset(crops, subset = uuid %in% parents, select = "uri"))
   rdfhelper::triple(
     uri,
-    rdfhelper::uri("parent", base),
+    rdfhelper::uri("http://schema.org/isPartOf"),
     parents
+  )
+
+  # link from observation set to observations
+  rdfhelper::triple(
+    rdfhelper::uri("https://cube.link/ObservationSet"),
+    rdfhelper::uri("https://cube.link/observation"),
+    uri
   )
 }
 sink()
